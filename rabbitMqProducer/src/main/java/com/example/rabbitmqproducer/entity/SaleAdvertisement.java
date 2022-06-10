dd3bb6c5dc9d0f74aac601545ab76c698e14e192
Gökhan Akpınar <gkhnkpnr@icloud.com>
2022-06-10 15:05:14 +0300

    DB Id problem Fixed.

--- rabbitMqProducer/src/main/java/com/example/rabbitmqproducer/entity/SaleAdvertisement.java
+++ rabbitMqProducer/src/main/java/com/example/rabbitmqproducer/entity/SaleAdvertisement.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 public class SaleAdvertisement {
     @Id
     @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "title")
--- rabbitMqProducer/src/main/java/com/example/rabbitmqproducer/entity/User.java
+++ rabbitMqProducer/src/main/java/com/example/rabbitmqproducer/entity/User.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 public class User {
     @Id
     @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "username")
